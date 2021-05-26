3963c0f256261b18cacd7a3611216fb3f51b7551
di.smirnov <di.smirnov@corp.vk.com>
2021-05-26 15:35:38 +0300

    add more info

--- app/src/main/java/ru/daniismirnov/deviceinfowallpaper/CoolPainter.kt
+++ app/src/main/java/ru/daniismirnov/deviceinfowallpaper/CoolPainter.kt
@@ -1,6 +1,7 @@
 package ru.daniismirnov.deviceinfowallpaper
 
 import android.annotation.SuppressLint
+import android.app.ActivityManager
 import android.content.Context
 import android.content.Context.WIFI_SERVICE
 import android.content.res.Resources
@@ -9,6 +10,9 @@ import android.net.wifi.WifiManager
 import android.os.Build
 import android.text.format.Formatter.formatIpAddress
 import android.util.DisplayMetrics
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 
 
 class CoolPainter {
@@ -18,7 +22,7 @@ class CoolPainter {
 
         paint.color = Color.WHITE
         paint.textSize = getScreenSize().widthPixels.toFloat() / 20
-        var y = (getScreenSize().heightPixels.toFloat() / 2) - paint.textSize * 5
+        var y = (getScreenSize().heightPixels.toFloat() / 2) - paint.textSize * 7
         val x = 5F
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
             canvas.drawText("Serial number: " + getSerial(), x, y, paint)
@@ -31,11 +35,15 @@ class CoolPainter {
         canvas.drawText("Model: " + getModel(), x, y, paint)
         y += paint.textSize
         canvas.drawText("Android " + getAndroidVersion(), x, y, paint)
-
+        y += paint.textSize
+        canvas.drawText("RAM: " + getRAMInfo(context), x, y, paint)
+        y += paint.textSize
+        canvas.drawText("Cores: " + getCores() + " Frequency: " + getCPUFrequency(), x, y, paint)
+        y += paint.textSize
 
     }
 
-    private fun drawBackground(canvas: Canvas){
+    private fun drawBackground(canvas: Canvas) {
         val paint = Paint()
 
         paint.color = Color.WHITE
@@ -81,4 +89,33 @@ fun getAndroidVersion(): String {
 fun getIP(context: Context): String {
     val wm = context.applicationContext.getSystemService(WIFI_SERVICE) as WifiManager?
     return formatIpAddress(wm!!.connectionInfo.ipAddress)
+}
+
+fun getRAMInfo(context: Context): String {
+    val actManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    val memInfo = ActivityManager.MemoryInfo()
+    actManager.getMemoryInfo(memInfo)
+    return (memInfo.totalMem / 1048576).toString() + "gb"
+}
+
+fun getCores(): String {
+    return Runtime.getRuntime().availableProcessors().toString()
+}
+
+fun getCPUFrequency(): String {
+    val proc = Runtime.getRuntime().exec("cat sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq")
+    val `is`: InputStream = proc.inputStream
+    return getStringFromInputStream(`is`)
+}
+
+private fun getStringFromInputStream(`is`: InputStream): String {
+    val sb = StringBuilder()
+    val br = BufferedReader(InputStreamReader(`is`))
+    var line: String? = null
+
+    while (br.readLine().also { line = it } != null) {
+        sb.append(line)
+        sb.append("\n")
+    }
+    return sb.toString()
 }
